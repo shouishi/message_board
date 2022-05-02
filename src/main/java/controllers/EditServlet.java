8430ce4861491a1a2867c0332c66c52a737394e6
shouichi.inaba <inamondeath@yahoo.co.jp>
2022-05-02 14:06:47 +0900

    存在しないidに対する処理を追加

--- src/main/java/controllers/EditServlet.java
+++ src/main/java/controllers/EditServlet.java
@@ -38,13 +38,14 @@ public class EditServlet extends HttpServlet {
 
         em.close();
 
-        // メッセージ情報とセッションIDをリクエストスコープに登録
+     // メッセージ情報とセッションIDをリクエストスコープに登録
         request.setAttribute("message", m);
         request.setAttribute("_token", request.getSession().getId());
 
+        // メッセージデータが存在しているときのみ
         // メッセージIDをセッションスコープに登録
         if(m != null) {
-        request.getSession().setAttribute("message_id", m.getId());
+            request.getSession().setAttribute("message_id", m.getId());
         }
 
         RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
